647335cc93ff7e4268bb2ffce9fbfb54e394949c
agsamkin <a.g.samkin@gmail.com>
2022-07-07 21:09:10 +0300

    Added adapter pattern example

---
+++ src/adapter/DuckTestDrive.java
@@ -0,0 +1,21 @@
+package adapter;
+
+public class DuckTestDrive {
+
+    public static void main(String[] args) {
+
+        Duck duck = new MallardDuck();
+        duck.fly();
+        duck.quack();
+
+        Turkey turkey = new WildTurkey();
+        turkey.fly();
+        turkey.gobble();
+
+        TurkeyAdapter turkeyAdapter = new TurkeyAdapter(turkey);
+        turkeyAdapter.fly();
+        turkeyAdapter.quack();
+
+    }
+
+}
